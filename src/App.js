9b1416e273edad7efd2e6ec9b27bdab7f3069b5d
tuyent8 <minhtuyent8@gmail.com.>
2025-04-18 21:53:03 +0700

    Thêm slidebar với NavLink

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import VacationDaySharehoder from './pages/VacationDaySharehoder/VacationDayShar
 import VacationDayGender from './pages/VacationDayGender/VacationDayGender';
 import VacationDayEmploymentType from './pages/VacationDayEmploymentType/VacationDayEmploymentType';
 import VacationDayEthnicity from './pages/VacationDayEthnicity/VacationDayEthnicity';
+import BenefitSharehoder from './pages/BenefitSharehoder/BenefitSharehoder';
+import BenefitPlan from './pages/BenefitPlan/BenefitPlan';
 
 function App() {
     return (
@@ -30,6 +32,8 @@ function App() {
                 <Route path="/vacationday-gender" element={<VacationDayGender />} />
                 <Route path="/vacationday-employment-type" element={<VacationDayEmploymentType />} />
                 <Route path="/vacationday-ethnicity" element={<VacationDayEthnicity />} />
+                <Route path="/benifit-sharehoder" element={<BenefitSharehoder />} />
+                <Route path="/benefit-plan" element={<BenefitPlan />} />
             </Routes>
         </div>
     );
--- src/components/SlideBar/SlideBarComponent.jsx
+++ src/components/SlideBar/SlideBarComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './SlideBarComponent.scss';
+import { NavLink } from 'react-router-dom';
 
 const SlideBarComponent = () => {
     const [incomeDropdownOpen, setIncomeDropdownOpen] = useState(false);
@@ -13,12 +14,16 @@ const SlideBarComponent = () => {
     return (
         <div className="sidebar">
             <div className="sidebar-menu">
-                <div className="active">
-                    <a href="#"><i className="bi bi-house-door"></i> Dashboard</a>
+                <div>
+                    <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>
+                        <i className="bi bi-house-door"></i> Dashboard
+                    </NavLink>
                 </div>
 
                 <div>
-                    <a href="#"><i className="bi bi-envelope"></i> Messages</a>
+                    <NavLink to="#" onClick={(e) => e.preventDefault()}>
+                        <i className="bi bi-envelope"></i> Messages
+                    </NavLink>
                 </div>
 
                 <div>
@@ -28,11 +33,11 @@ const SlideBarComponent = () => {
                     </a>
                     {incomeDropdownOpen && (
                         <ul className="submenu">
-                            <li><a href="#">Theo cổ đông</a></li>
-                            <li><a href="#">Theo giới tính</a></li>
-                            <li><a href="#">Theo sắc tộc</a></li>
-                            <li><a href="#">Partime or FullTime</a></li>
-                            <li><a href="#">Theo phòng ban</a></li>
+                            <li><NavLink to="/income-sharehoder" className={({ isActive }) => isActive ? 'active' : ''}>Theo cổ đông</NavLink></li>
+                            <li><NavLink to="/income-gender" className={({ isActive }) => isActive ? 'active' : ''}>Theo giới tính</NavLink></li>
+                            <li><NavLink to="/income-ethnicity" className={({ isActive }) => isActive ? 'active' : ''}>Theo sắc tộc</NavLink></li>
+                            <li><NavLink to="/income-employment-type" className={({ isActive }) => isActive ? 'active' : ''}>Partime or FullTime</NavLink></li>
+                            <li><NavLink to="/income-department" className={({ isActive }) => isActive ? 'active' : ''}>Theo phòng ban</NavLink></li>
                         </ul>
                     )}
                 </div>
@@ -44,10 +49,10 @@ const SlideBarComponent = () => {
                     </a>
                     {leaveDropdownOpen && (
                         <ul className="submenu">
-                            <li><a href="#">Theo cổ đông</a></li>
-                            <li><a href="#">Theo giới tính</a></li>
-                            <li><a href="#">Theo sắc tộc</a></li>
-                            <li><a href="#">Partime or FullTime</a></li>
+                            <li><NavLink to="/vacationday-sharehoder" className={({ isActive }) => isActive ? 'active' : ''}>Theo cổ đông</NavLink></li>
+                            <li><NavLink to="/vacationday-gender" className={({ isActive }) => isActive ? 'active' : ''}>Theo giới tính</NavLink></li>
+                            <li><NavLink to="/vacationday-ethnicity" className={({ isActive }) => isActive ? 'active' : ''}>Theo sắc tộc</NavLink></li>
+                            <li><NavLink to="/vacationday-employment-type" className={({ isActive }) => isActive ? 'active' : ''}>Partime or FullTime</NavLink></li>
                         </ul>
                     )}
                 </div>
@@ -59,14 +64,16 @@ const SlideBarComponent = () => {
                     </a>
                     {benefitDropdownOpen && (
                         <ul className="submenu">
-                            <li><a href="#">Theo cổ đông</a></li>
-                            <li><a href="#">Theo gói quyền lợi</a></li>
+                            <li><NavLink to="/benefit-sharehoder" className={({ isActive }) => isActive ? 'active' : ''}>Theo cổ đông</NavLink></li>
+                            <li><NavLink to="/benefit-plan" className={({ isActive }) => isActive ? 'active' : ''}>Theo gói quyền lợi</NavLink></li>
                         </ul>
                     )}
                 </div>
 
                 <div>
-                    <a href="#"><i className="bi bi-box-arrow-right"></i> Logout</a>
+                    <NavLink to="/logout" className={({ isActive }) => isActive ? 'active' : ''}>
+                        <i className="bi bi-box-arrow-right"></i> Logout
+                    </NavLink>
                 </div>
             </div>
         </div>
