2eb65b0c73e697ad3dc1817105049f5aa45d1b39
tuyent8 <minhtuyent8@gmail.com.>
2025-04-20 14:55:17 +0700

    ✨ Thêm giao diện sidebar

--- src/pages/BenefitPlan/BenefitPlan.jsx
+++ src/pages/BenefitPlan/BenefitPlan.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SlideBarComponent from '../../components/SlideBar/SlideBarComponent'
+import './BenefitPlan.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BenefitPlan = () => {
     return (
@@ -7,7 +9,34 @@ const BenefitPlan = () => {
             <div className='sidebar'>
                 <SlideBarComponent />
             </div>
-        </div>)
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>John</td>
+                        <td>Doe</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default BenefitPlan
